9737053dd00a934c310ca68aa7863efc23aab216
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-01-18 22:03:57 -0500

    debug

--- get_masters.py
+++ get_masters.py
@@ -121,10 +121,10 @@ if str(response.status_code)[0] == '2':
 	#print relevant parameters from health
 	for unit in response_dict['Units']:
 		print('**DEBUG: unit is {0} and is a {1}'.format(unit, type(unit)))
-		print('Name: {0}			State: {1}'.format( response_dict[unit]['UnitName'], response_dict[unit]['Health'] ) )
+		print('Name: {0}			State: {1}'.format( response_dict['Units'][unit]['UnitName'], response_dict['Units'][unit]['Health'] ) )
 		if response_dict[unit]['Health']: #not 0 means unhealthy, print all children
 			for node in unit['Nodes']:
-				print('Name: {0}			IP: {1}		State: {2}'.format( response_dict[unit]['UnitName'], response_dict[unit][node]['IP'], response_dict['unit'][node]['Health'] ) )
+				print('Name: {0}			IP: {1}		State: {2}'.format( response_dict['Units'][unit]['UnitName'], response_dict['Units'][unit][node]['IP'], response_dict['Units'][unit][node]['Health'] ) )
 else:
 	print ('** ERROR: GET Health: {} \n'.format( error ) ) 	
 
