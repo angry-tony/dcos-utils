34fc0ab86d1405d455f1b071d64784d6a9b12050
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-01-20 17:02:13 -0500

    catch all connection exceptions

--- get_nodes.py
+++ get_nodes.py
@@ -37,7 +37,7 @@ DCOS_TOKEN=os.environ['DCOS_TOKEN']
 
 print('**DEBUG: DCOS_IP: {}'.format(DCOS_IP))
 print('**DEBUG: DCOS_TOKEN: {}'.format(DCOS_TOKEN))
-if DCOS_IP='' or TOKEN='':
+if DCOS_IP=='' or TOKEN=='':
 	print('** ERROR: required variables DCOS_IP: {0}, TOKEN: {1}, \
 		not set appropriately. Please set and re-run'.format(DCOS_IP,TOKEN))
 
